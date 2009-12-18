cb5d645d5582d80d4dc5d0e02bc38d4ff72d075e
Dominic Baggott <dominic.baggott@gmail.com>
2009-12-18 17:51:51 +0100

    Added a simple test runner.

---
+++ test/markdown.t.js
@@ -0,0 +1,42 @@
+const test = require('test'),
+      asserts = test.asserts,
+      fs = require( "fs-base" ),
+      markdown = require( "markdown" );
+
+// get the list of all test collections
+var fixtures = fs.list( "fixtures" );
+
+// get rid of the README
+fixtures = fixtures.filter( function( x ) ! x.match( /\/README$/ ) );
+
+for ( var f in fixtures ) {
+  var name = fixtures[ f ].substring( fixtures[ f ].lastIndexOf( "/" ) + 1 );
+  exports[ "test_" + name ] = function() {
+    // grab all the test files in this fixture
+    var tests = fs.list( fixtures[ f ] );
+
+    // filter to only the raw files
+    tests = tests.filter( function( x ) x.match( /\.text$/ ) );
+
+    // remove the extensions
+    tests = tests.map( function( x ) x.replace( /\.text$/, "" ) );
+
+    for ( var t in tests ) {
+      // load the raw text
+      var text = fs.rawOpen( tests[ t ] + ".text", "r" ).readWhole();
+
+      // load the target output
+      var html = fs.isFile( tests[ t ] + ".html" )
+        ? fs.rawOpen( tests[ t ] + ".html", "r" ).readWhole()
+        : fs.rawOpen( tests[ t ] + ".xhtml", "r" ).readWhole();
+
+      asserts.same( markdown.toHTML( text ),
+                    html,
+                    tests[ t ].substring( tests[ t ].lastIndexOf( "/" ) + 1 ) );
+    }
+  }
+}
+
+if ( require.main === module ) {
+  test.runner( exports );
+}

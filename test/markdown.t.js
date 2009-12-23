226fbe5e110792f31dfb4d9cc41b8e3191071744
Dominic Baggott <dominic.baggott@gmail.com>
2009-12-23 15:33:41 +0100

    Fixed bug in test runner due to no closure.

--- test/markdown.t.js
+++ test/markdown.t.js
@@ -1,7 +1,7 @@
 const test = require('test'),
       asserts = test.asserts,
       fs = require( "fs-base" ),
-      markdown = require( "markdown" );
+      markdown = require( "md" );
 
 // get the list of all test collections
 var fixtures = fs.list( module.resource.resolve("fixtures") );
@@ -10,36 +10,43 @@ var fixtures = fs.list( module.resource.resolve("fixtures") );
 fixtures = fixtures.filter( function( x ) ! x.match( /\/README$/ ) );
 
 for ( var f in fixtures ) {
-  var name = fixtures[ f ].substring( fixtures[ f ].lastIndexOf( "/" ) + 1 );
-  exports[ "test_" + name ] = function() {
-    // grab all the test files in this fixture
-    var tests = fs.list( fixtures[ f ] );
+  ( function( fixture ) {
+    var name = fixture.substring( fixture.lastIndexOf( "/" ) + 1 );
+    exports[ "test_" + name ] = function() {
+      // grab all the test files in this fixture
+      var tests = fs.list( fixture );
 
-    // filter to only the raw files
-    tests = tests.filter( function( x ) x.match( /\.text$/ ) );
+      // filter to only the raw files
+      tests = tests.filter( function( x ) x.match( /\.text$/ ) );
 
-    // remove the extensions
-    tests = tests.map( function( x ) x.replace( /\.text$/, "" ) );
+      // remove the extensions
+      tests = tests.map( function( x ) x.replace( /\.text$/, "" ) );
 
-    for ( var t in tests ) {
-      // load the raw text
-      var text = fs.rawOpen( tests[ t ] + ".text", "r" ).readWhole();
+      for ( var t in tests ) {
+        // load the raw text
+        var text_file = fs.rawOpen( tests[ t ] + ".text", "r" ),
+            text = text_file.readWhole();
+        text_file.close();
 
-      // load the target output
-      var html = fs.isFile( tests[ t ] + ".html" )
-        ? fs.rawOpen( tests[ t ] + ".html", "r" ).readWhole()
-        : fs.rawOpen( tests[ t ] + ".xhtml", "r" ).readWhole();
+        // load the target output
+        var html_file = fs.isFile( tests[ t ] + ".html" )
+          ? fs.rawOpen( tests[ t ] + ".html", "r" )
+          : fs.rawOpen( tests[ t ] + ".xhtml", "r" );
 
-      try {
-        asserts.same( markdown.toHTML( text ),
-                      html,
-                      tests[ t ].substring( tests[ t ].lastIndexOf( "/" ) + 1 ) );
-      }
-      catch( e ) {
-        asserts.ok( 0, "Couldn't parse " + tests[ t ].substring( tests[ t ].lastIndexOf( "/" ) + 1 ) + " -- " + e );
+        var html = html_file.readWhole();
+        html_file.close();
+
+        try {
+          asserts.same( markdown.toHtml( text ),
+                        html,
+                        tests[ t ].substring( tests[ t ].lastIndexOf( "/" ) + 1 ) );
+        }
+        catch( e ) {
+          asserts.ok( 0, "Couldn't parse " + tests[ t ].substring( tests[ t ].lastIndexOf( "/" ) + 1 ) + " -- " + e );
+        }
       }
     }
-  }
+  } )( fixtures[ f ] );
 }
 
 if ( require.main === module ) {

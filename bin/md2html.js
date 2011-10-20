d9235f50e2bbbf73e20780b9bd77acd678ee58e1
AJ ONeal <coolaj86@gmail.com>
2011-10-20 11:10:52 -0600

    added md2html

---
+++ bin/md2html.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+(function () {
+  "use strict";
+
+  var fs = require('fs')
+    , markdown = require('markdown').markdown
+    , fullpath = process.argv[2]
+    ;
+
+  function convert(err, data) {
+    var md
+      , html
+      ;
+
+    if (err) {
+      throw err;
+    }
+
+    md = data.toString('utf8');
+    html = markdown.toHTML(md);
+    console.log(html);
+  }
+
+  if (!fullpath) {
+    console.error('try: ', process.argv[1].split('/').pop(), '/path/to/doc.md');
+    return;
+  }
+
+  console.log(markdown);
+
+  fs.readFile(fullpath, convert);
+}())

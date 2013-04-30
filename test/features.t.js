3553769ba8ec31572998bf1666814d08f0dd186a
Dominic Baggott <dominic.baggott@gmail.com>
2013-04-30 14:09:18 +0100

    Refactor features.t.js to use Mocha/Chai

--- test/features.t.js
+++ test/features.t.js
@@ -1,82 +1,81 @@
-var markdown = require("../lib/markdown");
+// require modules under node.js; in a browser they'll already be present
+if ( typeof chai === "undefined" ) { var chai = require( "chai" ); }
+if ( typeof markdown === "undefined" ) { var markdown = require( "../lib/markdown" ); }
 
-function test_dialect( dialect, features ) {
-  var fs = require("fs"),
-      path = require("path"),
-      tap = require("tap");
+var assert = chai.assert;
 
-  var slurpFile = function slurpFile( path ) {
-    return fs.readFileSync( path, "utf8" );
-  }
-
-  var isFile = function isFile( f ) {
-    try {
-      return fs.statSync( f ).isFile()
-    }
-    catch (e) {
-      if ( e.code == "ENOENT" ) return false;
-      throw e;
-    }
-  };
-
-
-  tap.test( dialect, function(tap) {
-    for ( var f in features ) {
-      (function( feature ) {
-        tap.test( feature, function(tap) {
-          var test_path = path.join(__dirname, "features", feature);
-
-          // grab all the test files in this feature
-          var tests = fs.readdirSync( test_path );
-
-          // filter to only the raw files
-          tests = tests.filter( function( x ) {return x.match( /\.text$/ ); } );
-
-          // remove the extensions
-          tests = tests.map( function( x ) {return x.replace( /\.text$/, "" ); } );
-
-          for ( var t in tests ) {
-            // load the raw text
-            var testName = dialect + "/" + feature + "/" + tests[ t ].substring( tests[ t ].lastIndexOf( "/" ) + 1 ),
-                testFileBase = path.join(test_path, tests[ t ]);
-                text = slurpFile( testFileBase + ".text" );
-
-            // load the target output
-            var json = JSON.parse( slurpFile( testFileBase + ".json" ) );
-
-            var output = markdown.toHTMLTree( text, dialect );
-            tap.equivalent( output, json, testName, {todo: isFile( testFileBase + ".todo" )} );
-          }
-          tap.end();
-        } );
-      } )( features[ f ] );
-    }
-  });
+// Utilities to load feature test-case input/output files
+if ( typeof require === "undefined" ) {
+  var feature_path = function( feature ) {
+        return "features/" + feature;
+      },
+      list_tests = function( feature_path ) {
+        console.log( feature_path );
+      },
+      test_path = function( feature_path, test ) {
+        return feature_path + "/" + test;
+      },
+      slurp_test_files = function( test_path ) {
+      };
+}
+else {
+  var fs = require( "fs" ),
+      path = require( "path" ),
+      feature_path = function( feature ) {
+        return path.join( __dirname, "features", feature );
+      },
+      list_tests = function( feature_path ) {
+        return fs.readdirSync( feature_path )
+                 .filter( function( f ) { return f.match( /\.text$/ ); } )
+                 .map( function( f ) { return f.replace( /\.text$/, "" ); } );
+      },
+      test_path = function( feature_path, test ) {
+        return path.join( feature_path, test );
+      },
+      slurp_test_files = function( test_path ) {
+        return {
+          text: fs.readFileSync( test_path + ".text", "utf8" ),
+          json: JSON.parse( fs.readFileSync( test_path + ".json", "utf8" ) )
+        };
+      };
 }
 
-
-
-var dialects = {};
-dialects.Gruber = [
-  "blockquotes",
-  "code",
-  "emphasis",
-  "headers",
-  "horizontal_rules",
-  "images",
-  "linebreaks",
-  "links",
-  "lists"
-];
+var dialects = {
+  Gruber: [
+    "blockquotes",
+    "code",
+    "emphasis",
+    "headers",
+    "horizontal_rules",
+    "images",
+    "linebreaks",
+    "links",
+    "lists"
+  ]
+};
 
 dialects.Maruku = dialects.Gruber.slice( 0 );
 dialects.Maruku.push( "meta", "definition_lists" );
 
-// TODO if dialects/features were passed on the command line, filter to them
-// if ( args.length ) {
-//   features = features.filter( function( x ) args.indexOf( x ) !== -1 );
-// }
-
 for ( var d in dialects ) {
-  test_dialect( d, dialects[ d ] );
+  suite( d, function() {
+    for ( var feature in dialects[ d ] ) {
+      suite( dialects[ d ][ feature ], function() {
+        test_feature( d, dialects[ d ][ feature ] );
+      } );
+    }
+  } );
+}
+
+function test_feature( dialect, feature ) {
+  var path = feature_path( feature ),
+      tests = list_tests( path );
+
+  for ( var t in tests ) {
+    var files = slurp_test_files( test_path( path, tests[ t ] ) );
+
+    test( tests[ t ], function() {
+      assert.deepEqual( files.json, markdown.toHTMLTree( files.text, dialect ) );
+    } );
+  }
 }

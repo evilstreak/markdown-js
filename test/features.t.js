ac9de008960e949b78d5abfa32c45c09fc8855eb
XhmikosR <xhmikosr@users.sourceforge.net>
2013-08-16 10:23:52 +0300

    JSHint: Enable curly rule.

--- test/features.t.js
+++ test/features.t.js
@@ -14,7 +14,9 @@ function test_dialect( dialect, features ) {
       return fs.statSync( f ).isFile();
     }
     catch (e) {
-      if ( e.code === "ENOENT" ) return false;
+      if ( e.code === "ENOENT" ) {
+        return false;
+      }
       throw e;
     }
   };

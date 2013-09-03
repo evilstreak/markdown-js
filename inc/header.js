2b947247ff7ee2c39a99a2e2b07fcfd285c6722f
Robin Ward <robin.ward@gmail.com>
2013-09-03 12:28:46 -0400

    Split up markdown-js into multiple files, uses Grunt to produce browser distribution.

---
+++ inc/header.js
@@ -0,0 +1,7 @@
+// Released under MIT license
+// Copyright (c) 2009-2010 Dominic Baggott
+// Copyright (c) 2009-2010 Ash Berlin
+// Copyright (c) 2011 Christoph Dorn <christoph@christophdorn.com> (http://www.christophdorn.com)
+// Date: @DATE
+
+(function(expose) {

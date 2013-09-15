ea47f3b73c0762c316bda166ee622adf902032f9
Ash Berlin <ash_github@firemirror.com>
2013-09-15 14:43:45 +0100

    Replace tabs with spaces

--- inc/footer-node.js
+++ inc/footer-node.js
@@ -1,8 +1,8 @@
 
-	expose.Markdown = Markdown;
-	expose.parse = Markdown.parse;
-	expose.toHTML = Markdown.toHTML;
-	expose.toHTMLTree = Markdown.toHTMLTree;
-	expose.renderJsonML = Markdown.renderJsonML;
+  expose.Markdown = Markdown;
+  expose.parse = Markdown.parse;
+  expose.toHTML = Markdown.toHTML;
+  expose.toHTMLTree = Markdown.toHTMLTree;
+  expose.renderJsonML = Markdown.renderJsonML;
 
 })(exports);

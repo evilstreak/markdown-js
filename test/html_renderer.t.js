3e5e086912adcc8566a7c2c105b42de07664e6f3
Robin Ward <robin.ward@gmail.com>
2013-10-01 15:17:09 -0400

    Supports disabling of HTML sanitization via `sanitize: false` option.

--- test/html_renderer.t.js
+++ test/html_renderer.t.js
@@ -6,3 +6,17 @@ tap.test("src attribute order", function(t) {
   t.equivalent( tree, '<p><img src="/images/photo.jpg" alt="photo"/></p>' );
   t.end();
 });
+
+tap.test("HTML Sanitizing", function(t) {
+	var input = "hello <world>";
+
+  t.equivalent( markdown.toHTML(input, 'Gruber'),
+                '<p>hello &lt;world&gt;</p>',
+                "escapes by default" );
+
+  t.equivalent( markdown.toHTML(input, 'Gruber', {sanitize: false}),
+                '<p>hello <world></p>',
+                "sanitization can be disabled" );
+
+  t.end();
+});

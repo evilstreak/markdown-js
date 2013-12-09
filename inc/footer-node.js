2dbb83783683a3c249a8608b4cd4375bcd03517c
Robin Ward <robin.ward@gmail.com>
2013-12-09 15:16:03 -0500

    Expose `DialectHelpers` so people can include markdown-js and then add their own dialects.

--- inc/footer-node.js
+++ inc/footer-node.js
@@ -4,5 +4,6 @@
   expose.toHTML = Markdown.toHTML;
   expose.toHTMLTree = Markdown.toHTMLTree;
   expose.renderJsonML = Markdown.renderJsonML;
+  expose.DialectHelpers = DialectHelpers;
 
 })(exports);
--- inc/footer-web.js
+++ inc/footer-web.js
@@ -4,6 +4,7 @@
   expose.toHTML = Markdown.toHTML;
   expose.toHTMLTree = Markdown.toHTMLTree;
   expose.renderJsonML = Markdown.renderJsonML;
+  expose.DialectHelpers = DialectHelpers;
 
 })(function() {
   window.markdown = {};

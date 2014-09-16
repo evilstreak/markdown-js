7fb0fbadd9a704d72e98bb86aa44db3236d789a6
Melvin Tiong <cy.tiong@gmail.com>
2014-09-16 19:41:20 +0800

    Write a mutation check for toHTMLTree and renderJsonML (renderJsonML fails)

--- test/render_tree.t.js
+++ test/render_tree.t.js
@@ -1,6 +1,12 @@
 var markdown = require("../src/markdown"),
     tap = require("tap");
 
+function clone_array( input ) {
+  // Helper method. Since the objects are plain round trip through JSON to get
+  // a clone
+  return JSON.parse( JSON.stringify( input ) );
+}
+
 tap.test("undefined attribute", function(t) {
   var tree = markdown.renderJsonML( ['html', ['p', {style: undefined }, 'hello'] ] );
   t.equivalent( tree, '<p>hello</p>' );
@@ -10,5 +16,19 @@ tap.test("undefined attribute", function(t) {
 tap.test("escaped attribute", function(t) {
   var tree = markdown.renderJsonML( ['html', ['p', {style: "color: blue" }, 'hello'] ] );
   t.equivalent( tree, '<p style="color: blue">hello</p>' );
+  t.end();
+});
+
+tap.test("intermediate trees left alone", function(t) {
+  var markdownTree = [ [ "bulletlist", [ "listitem", "foo\nbaz" ], [ "listitem", "bar\nbaz" ] ] ];
+  var cloneMarkdownTree = clone_array( markdownTree );
+  markdown.toHTMLTree( markdownTree );
+  t.equivalent( markdownTree, cloneMarkdownTree, 'toHTMLTree should not mutate its argument' );
+
+  var htmlTree = ['html', ['p', {style: "color: blue" }, 'hello'] ];
+  var cloneHtmlTree = clone_array( htmlTree );
+  markdown.renderJsonML( htmlTree );
+  t.equivalent( htmlTree, cloneHtmlTree, 'renderJsonML should not mutate its argument' );
+
   t.end();
 });

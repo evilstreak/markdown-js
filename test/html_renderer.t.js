1a0d5e7ad28b16cbb58af3bf3b82d5db94635d42
Marvin Kastner <1kastner@informatik.uni-hamburg.de>
2015-07-12 13:55:48 +0200

    Test my code

--- test/html_renderer.t.js
+++ test/html_renderer.t.js
@@ -6,3 +6,19 @@ tap.test("src attribute order", function(t) {
   t.equivalent( tree, '<p><img src="/images/photo.jpg" alt="photo"/></p>' );
   t.end();
 });
+
+tap.test("standalone", function(t) {
+  var tree = markdown.toHTML("", "Gruber", {"standalone": true});
+  t.equivalent( tree, "<!doctype html><head>\n\n</head><body>\n\n</body>" );
+  t.end();
+});
+
+tap.test("standalone plus utf8", function(t) {
+  var tree = markdown.toHTML("", "Gruber", {"standalone": true, "htmlencoding": "utf8"});
+  t.equivalent( tree, "<!doctype html><head>\n\n<meta charset='utf-8'/>\n\n</head><body>\n\n</body>" );
+
+  var tree2 = markdown.toHTML("# test", "Gruber", {"standalone": true, "htmlencoding": "utf8"});
+  t.equivalent( tree2, "<!doctype html><head>\n\n<meta charset='utf-8'/>\n\n</head><body>\n\n<h1>test</h1>\n\n</body>" );
+
+  t.end();
+});

6fd0c774236c19ee0c450b05c7bf8171b0fae9d1
Greg Bowler <greg.bowler@g105b.com>
2015-08-11 17:06:57 +0100

    Create component.js

---
+++ component.js
@@ -0,0 +1,11 @@
+{
+  "name": "markdown-js",
+  "description": "A sensible Markdown parser for javascript",
+  "main": "./lib/index.js",
+  "keywords": [
+    "markdown",
+    "text processing",
+    "ast"
+  ],
+  "license": "MIT"
+}
